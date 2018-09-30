4bb12dd3282140c5d75556c1b35a7714514ca17e
Chaltin Pagan <chaltinpagan@gmail.com>
2018-09-30 14:23:56 -0400

    Removing unneeded coe form routes

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
-const path = require('path');
-const bodyParser = require('body-parser');
 const app = express();
 const axios = require('axios');
 const key = require('./env.js');
 const port = 8000;
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res, next) {
-    // res.sendFile(path.join(__dirname, 'routes.html'));
     res.send("Open Weather Map Routes");
 });
 
